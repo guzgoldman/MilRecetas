fc08acf749183194b5ac1ce683b217ef775eb274
Héctor Guzman <107734309+guzgoldman@users.noreply.github.com>
2024-05-15 16:28:10 -0300

    Validación de formularios en JS creada

---
+++ js/script.js
@@ -0,0 +1,29 @@
+document.getElementById('login-button').addEventListener('click', function(event) {
+    var username = document.getElementById('username-input');
+    var password = document.getElementById('password-input');
+    var errorMessage = document.getElementById('error-message');
+
+    errorMessage.textContent = "";
+
+    if(username.value.trim().length < 8 || username.value.trim().length > 32 || password.value.trim().length < 8) {
+        event.preventDefault();
+
+        if(username.value.trim().length < 8 || username.value.trim().length > 32) {
+            username.style.borderColor = "red";
+            errorMessage.textContent = "El nombre de usuario es incorrecto o no ha sido ingresado.";
+        } else {
+            username.style.borderColor = "";
+        }
+
+        if(password.value.trim().length < 8) {
+            password.style.borderColor = "red";
+            errorMessage.textContent = "La contraseña es incorrecta o no ha sido ingresada.";
+        } else {
+            password.style.borderColor = "";
+        }
+
+        if((username.value.trim().length < 8 || username.value.trim().length > 32) && password.value.trim().length < 8) {
+            errorMessage.textContent = "El nombre de usuario y la contraseña son incorrectos o han sido ingresados.";
+        }
+    }
+});
